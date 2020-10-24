d8a78828b3a5ea30edf1909c602e7527eb78fcc7
conniez99 <49135897+conniez99@users.noreply.github.com>
2020-10-24 15:14:44 -0400

    water sound

---
+++ Assets/PlayerAudio.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerAudio : MonoBehaviour
+{
+    public AudioClip Swimming;
+    public AudioSource audioS;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Water"))
+        {
+            print("in");
+            audioS.clip = Swimming;
+            audioS.Play();
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.CompareTag("Water"))
+        {
+            print("out");
+            audioS.clip = Swimming;
+            audioS.Stop();
+        }
+    }
+}
+
+
