ab20efe909689326389e43fcdf3c336285b466ba
conniez99 <49135897+conniez99@users.noreply.github.com>
2020-11-05 21:49:17 -0500

    pickups

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class PlayerController : MonoBehaviour
+{
+    public TextMeshProUGUI countText;
+    private int count;
+    public GameObject winTextObject;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void SetCountText()
+    {
+        countText.text = "Collected: " + count.ToString() + " of 15";
+        if (count >= 20)
+        {
+            winTextObject.SetActive(true);
+        }
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.CompareTag("PickUp"))
+        {
+            other.gameObject.SetActive(false);
+            count = count + 1;
+
+            SetCountText();
+        }
+    }
+
+}
